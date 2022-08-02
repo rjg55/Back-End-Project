fba2255679de374eba0e2a6eb2fbd8a4606734c7
rjg55 <rikgraham55@gmail.com>
2022-08-02 15:52:35 +0100

    remove console.logs

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.use("*", (req, res) => {
 // Error Handling //
 
 app.use((err, req, res, next) => {
-  console.log(err);
   if (err.status && err.msg) {
     res.status(err.status).send({ status: err.status, msg: err.msg });
   } else if (err.code === "22P02") {
