0574ecea57f9a01a25eedbe7bfb2f13ce46ab678
rjg55 <rikgraham55@gmail.com>
2022-08-02 15:51:49 +0100

    add comment_count property to article response object with test

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use("*", (req, res) => {
 // Error Handling //
 
 app.use((err, req, res, next) => {
+  console.log(err);
   if (err.status && err.msg) {
     res.status(err.status).send({ status: err.status, msg: err.msg });
   } else if (err.code === "22P02") {
--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -4,6 +4,7 @@ exports.getArticlesById = (req, res, next) => {
   const { article_id } = req.params;
   selectArticleById(article_id)
     .then((article) => {
+      console.table(article);
       res.status(200).send({ article: article });
     })
     .catch(next);
