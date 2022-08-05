47696dcf136480f2b29d9f64ead02159651f4876
rjg55 <rikgraham55@gmail.com>
2022-08-05 10:56:17 +0100

    add error case to listen.js

--- listen.js
+++ listen.js
@@ -2,4 +2,9 @@ const app = require("./app");
 
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+app.listen(PORT, () => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on ${PORT}...`);
+});
